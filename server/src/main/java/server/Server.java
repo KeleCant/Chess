cd8622b52725dc0e0d823a340ce94f5a74ddb9a1
kelecant <Kelecant@gmail.com>
2024-02-24 13:45:15 -0700

    Updated DAOs to Interfeces

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -5,11 +5,12 @@ import spark.*;
 public class Server {
 
     public int run(int desiredPort) {
-        Spark.port(desiredPort);
+        Spark.port(8080);
 
         Spark.staticFiles.location("web");
-
+        //Spark.externalStaticFileLocation("C:\\Users\\kelec\\CS 240\\chess\\web");
         // Register your endpoints and handle exceptions here.
+        Spark.delete("/db", (req, res) -> (new ClearHandler()).clear(req, res));
 
         Spark.awaitInitialization();
         return Spark.port();
