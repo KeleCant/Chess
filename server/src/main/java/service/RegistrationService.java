4442dfe85e54c86ebd850e1713f5bb2405fa84ed
kelecant <Kelecant@gmail.com>
2024-02-24 18:08:06 -0700

    RegistrationService updated

--- server/src/main/java/service/RegistrationService.java
+++ server/src/main/java/service/RegistrationService.java
@@ -1,18 +1,19 @@
 package service;
 
 import dataAccess.*;
+import requests.RegistrationRequest;
+import results.RegistrationResult;
 
 public class RegistrationService {
 
-    public String register (String username, String password, String email) throws DataAccessException {
+    public RegistrationResult register (RegistrationRequest request) throws DataAccessException {
         UserDAO UserDAO = new UserDAOMemory();
         AuthDAO AuthDAO = new AuthDAOMemory();
 
         //check to see if username is taken
-        UserDAO.checkUsername(username);
+        UserDAO.checkUsername(request.username());
 
-        UserDAO.createUser(username, password,email);
-        return AuthDAO.createAuth(username);
+        UserDAO.createUser(request.username(), request.password(), request.email());
+        return new RegistrationResult(request.username(), AuthDAO.createAuth(request.username()));
     }
-
 }
