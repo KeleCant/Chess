f7d4f26e9610c5391e177fb9a84995e134959059
Kele Kent <94769317+KeleCant@users.noreply.github.com>
2024-03-07 03:32:56 -0700

    UserDAO done

--- server/src/test/java/dataAccessTests/SQLGameDAOTest.java
+++ server/src/test/java/dataAccessTests/SQLGameDAOTest.java
@@ -1,18 +1,17 @@
 package dataAccessTests;
 
-import dataAccess.DataAccessException;
-import dataAccess.GameDAO;
-import dataAccess.SQLGameDAO;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
+import dataAccess.*;
+import org.junit.jupiter.api.*;
 import passoffTests.testClasses.TestException;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SQLGameDAOTest {
     GameDAO gameDAO = new SQLGameDAO();
+    AuthDAO authDAO = new SQLAuthDAO();
+    int testID;
+    String testAuth;
 
     public SQLGameDAOTest() throws DataAccessException {
     }
@@ -22,15 +21,55 @@ public class SQLGameDAOTest {
         gameDAO.clear();
         gameDAO.createGame("F");
         gameDAO.createGame("U");
-        gameDAO.createGame("N");
+        testID = gameDAO.createGame("N");
+        testAuth = authDAO.createAuth("Sam");
 
     }
 
     @Test
     @Order(1)
-    @DisplayName("Clear Service")
-    public void clearService() throws Exception {
+    @DisplayName("Clear")
+    public void clear() throws Exception {
         gameDAO.clear();
         assertEquals(false, gameDAO.doesGameExist(1));
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("updateGame")
+    public void updateGame() throws Exception {
+        assertDoesNotThrow(() -> gameDAO.updateGame(testAuth, testID, "WHITE", authDAO));
+        assertDoesNotThrow(() -> gameDAO.updateGame(testAuth, testID, "BLACK", authDAO));
+        assertDoesNotThrow(() -> gameDAO.updateGame(testAuth, testID, null, authDAO));
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("updateGameNegative")
+    public void updateGameNegative() throws Exception {
+        assertDoesNotThrow(() -> gameDAO.updateGame(testAuth, testID, "BLACK", authDAO));
+        Exception exp = Assertions.assertThrows(DataAccessException.class, () -> gameDAO.updateGame(testAuth, testID, "BLACK", authDAO));
+        assertEquals("Error: already taken", exp.getMessage());
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("doesGameExist")
+    public void doesgameExist() throws Exception {
+        assertEquals(true, gameDAO.doesGameExist(testID), "game not read properly");
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("doesGameExistNegative")
+    public void doesgameExistNegative() throws Exception {
+        assertEquals(false, gameDAO.doesGameExist(0), "Game read improperluy");
+    }
+
+    @Test
+    @Order(6)
+    @DisplayName("listgames")
+    public void listgames() throws Exception {
+        assertEquals(3, gameDAO.listGame().size(), "Size is incorrect");
     }
 }
