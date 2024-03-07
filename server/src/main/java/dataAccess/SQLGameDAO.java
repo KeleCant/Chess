00c5cb57b50d246b12ee92872f6c8056802de66a
Kele Kent <94769317+KeleCant@users.noreply.github.com>
2024-03-06 18:39:20 -0700

    Saw TA and got tables functioning

--- server/src/main/java/dataAccess/SQLGameDAO.java
+++ server/src/main/java/dataAccess/SQLGameDAO.java
@@ -1,9 +1,13 @@
 package dataAccess;
 
+import chess.ChessGame;
+import com.google.gson.Gson;
 import model.GameData;
 
 import java.sql.SQLException;
 import java.util.HashSet;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
 
 public class SQLGameDAO implements GameDAO {
 
@@ -22,7 +26,7 @@ public class SQLGameDAO implements GameDAO {
               `whiteusername` varchar(256),
               `blackusername` varchar(256),
               `gamename` varchar(256),
-              `chessgame` TEXT,
+              `chessgame` JSON NOT NULL,
               PRIMARY KEY (`gameid`),
               INDEX(gamename)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
@@ -51,7 +55,26 @@ public class SQLGameDAO implements GameDAO {
     //creates and imports new game into database
     @Override
     public int createGame(String gameName) {
-        return 0;
+        int gameID;
+        ChessGame chessGame = new ChessGame();
+        try (var con = DatabaseManager.getConnection()) {
+            try (var preparedStatement = con.prepareStatement("INSERT INTO game (gameName, whiteUsername, blackUsername, game) VALUES (?, ?, ?, ?)", RETURN_GENERATED_KEYS)) {
+                preparedStatement.setString(1, gameName);
+                preparedStatement.setString(2, null);
+                preparedStatement.setString(3, null);
+                preparedStatement.setObject(4, new Gson().toJson(chessGame));
+                preparedStatement.executeUpdate();
+                try (var rs = preparedStatement.getGeneratedKeys()) {
+                    if (rs.next()) {
+                        // Retrieve the auto-increment key
+                        gameID = rs.getInt(1);
+                    } else {
+                        throw new SQLException("No auto-generated keys were returned.");
+                    }
+                }
+            }
+        } catch (DataAccessException | SQLException exception) { throw new RuntimeException(exception); }
+        return gameID;
     }
     @Override
     public void updateGame(String authToken, int gameID, String clientColor, AuthDAO authDAO) throws DataAccessException {
@@ -67,19 +90,14 @@ public class SQLGameDAO implements GameDAO {
     }
     @Override
     public HashSet<GameData> listGame() {
-//        var result = new HashSet<GameData>();
-//        try (var con = DatabaseManager.getConnection()) {
-//            try (var ps = con.prepareStatement("SELECT * FROM gameDataTable")) {
-//                try (var rs = ps.executeQuery()) {
-//                    while (rs.next()) {
-//
-//                        //result.add(readGame(rs));
-//                    }
-//                }
-//            }
-//        } catch (Exception exception) { throw new RuntimeException(exception); }
-//        return result;
-//    }
-        return null;
+        HashSet<GameData> games = new HashSet<>();
+        try (var con = DatabaseManager.getConnection(); var preparedStatement = con.prepareStatement("SELECT * FROM gameDataTable")) {
+            try (var resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    games.add(new GameData(resultSet.getInt("gameid"), resultSet.getString("whiteusername"), resultSet.getString("blackusername"), resultSet.getString("gamename"), new Gson().fromJson(resultSet.getString("chessgame"), ChessGame.class)));
+                }
+                return games;
+            }
+        } catch (DataAccessException | SQLException exception) { throw new RuntimeException(exception); }
     }
 }
