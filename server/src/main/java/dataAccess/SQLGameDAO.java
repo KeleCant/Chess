2274989413a94f0fb85a7fb7e910e6369ff5a35c
Kele Kent <94769317+KeleCant@users.noreply.github.com>
2024-03-06 22:49:39 -0700

    Saw TA and got tables functioning

--- server/src/main/java/dataAccess/SQLGameDAO.java
+++ server/src/main/java/dataAccess/SQLGameDAO.java
@@ -20,7 +20,7 @@ public class SQLGameDAO implements GameDAO {
             String[] createStatements = {
                     """
             CREATE TABLE IF NOT EXISTS gameDataTable (
-              `gameid` int NOT NULL AUTO_INCREMENT,
+              `gameid` int NOT NULL,
               `whiteusername` varchar(256),
               `blackusername` varchar(256),
               `gamename` varchar(256),
@@ -53,10 +53,12 @@ public class SQLGameDAO implements GameDAO {
     //creates and imports new game into database
     @Override
     public int createGame(String gameName) {
+//        if (game.gameID() < 0 || game.gameName() == null || game.game() == null)
+//            throw new DataAccessException("Invalid game data");
 //        int gameID;
 //        ChessGame chessGame = new ChessGame();
 //        try (var con = DatabaseManager.getConnection()) {
-//            try (var preparedStatement = con.prepareStatement("INSERT INTO game (gameName, whiteUsername, blackUsername, game) VALUES (?, ?, ?, ?)", RETURN_GENERATED_KEYS)) {
+//            try (var preparedStatement = con.prepareStatement("INSERT INTO game (gameName, whiteUsername, blackUsername, game) VALUES (?, ?, ?, ?)")) {
 //                preparedStatement.setString(1, gameName);
 //                preparedStatement.setString(2, null);
 //                preparedStatement.setString(3, null);
@@ -78,6 +80,33 @@ public class SQLGameDAO implements GameDAO {
     @Override
     public void updateGame(String authToken, int gameID, String clientColor, AuthDAO authDAO) throws DataAccessException {
 
+//        GameData game = gameDataList.get(gameID);
+//
+//        if (clientColor == null){
+//            //join as spectator
+//            game = new GameData(game.gameID(), game.whiteUsername(), game.blackUsername(), game.gameName(), game.game());
+//            gameDataList.put(gameID, game);
+//        }
+//        else if (clientColor.equals("WHITE")) {   //add White player
+//            //check to see if color is taken
+//            if (game.whiteUsername() != null)
+//                throw new DataAccessException("Error: already taken");
+//
+//            game = new GameData(game.gameID(), authDAO.getUsername(authToken), game.blackUsername(), game.gameName(), game.game());
+//            gameDataList.put(gameID, game);
+//        }
+//        else if (clientColor.equals("BLACK")) {  //add Black player
+//            //check to see if color is taken
+//            if (game.blackUsername() != null)
+//                throw new DataAccessException("Error: already taken");
+//
+//            game = new GameData(game.gameID(), game.whiteUsername(), authDAO.getUsername(authToken), game.gameName(), game.game());
+//            gameDataList.put(gameID, game);
+//        }
+//        else {
+//            throw new DataAccessException("Error: bad request");
+//        }
+
     }
 
     /*
@@ -85,7 +114,16 @@ public class SQLGameDAO implements GameDAO {
      */
     @Override
     public boolean doesGameExist(int gameID) {
-        return false;
+        try (var con = DatabaseManager.getConnection(); var preparedStatement = con.prepareStatement("SELECT * FROM game WHERE gameID=?")) {
+            preparedStatement.setInt(1, gameID);
+            try (var rs = preparedStatement.executeQuery()) {
+                if (rs.next()) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        } catch (DataAccessException | SQLException exception) { throw new RuntimeException(exception); }
     }
     @Override
     public HashSet<GameData> listGame() {
