499866bc0b7182ba33c92c7fe69594ee1503c4da
Kele Kent <94769317+KeleCant@users.noreply.github.com>
2024-03-19 21:08:47 -0600

    Set up logged in stuff

--- client/src/main/java/ui/ClientMenu.java
+++ client/src/main/java/ui/ClientMenu.java
@@ -48,6 +48,64 @@ public class ClientMenu {
         } else if (input.contains("Quit") || input.contains("quit")) {
             identity = "Exit";
         } else if (input.contains("Login") || input.contains("login")) {
+            login(input);
+        } else if (input.contains("Register") || input.contains("register")) {
+            register(input);
+        } else {
+            System.out.println("Invalid Input - Type \"Help\" for a list of commands or \"Quit\" to exit the program.");
+        }
+    }
+
+        //Help, Create Game, List Games, Join Game, Join Observer
+        public void postLoginClient () {
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+
+            if (input.contains("Help") || input.contains("help")) {
+                System.out.println("Create <GAMENAME> - Creates a new Game");
+                System.out.println("list - Lists Every Game");
+                System.out.println("Join <ID> [WHITE|BLACK|<empty>] - Joins a game");
+                System.out.println("Observe <ID> - Joins game as a spectator");
+                System.out.println("Logout - Log out of your account");
+                System.out.println("Quit - closes the program");
+                System.out.println("Help - list possible commands");
+            } else if (input.contains("Logout") || input.contains("logout")) {
+                logout();
+            } else if (input.contains("Create") || input.contains("create")) {
+                create(input);
+            } else if (input.contains("List") || input.contains("list")) {
+                list();
+            } else if (input.contains("Join") || input.contains("join")) {
+                join(input);
+            } else if (input.contains("Observe") || input.contains("observe")) {
+                observe(input);
+            } else if (input.contains("Quit") || input.contains("quit")) {
+                logout();
+                identity = "Exit";
+            } else {
+                System.out.println("Invalid Input - Type \"Help\" for a list of commands or \"Quit\" to exit the program.");
+            }
+        }
+
+        //
+        public void gamePlayClient () {
+
+        }
+
+        private void returnErrorMessage (String errorCode){
+            if (errorCode.contains("400"))
+                System.out.println("Error: bad request");
+            else if (errorCode.contains("401"))
+                System.out.println("Error: unauthorized");
+            else if (errorCode.contains("403"))
+                System.out.println("Error: already taken");
+            else if (errorCode.contains("500"))
+                System.out.println("Error: bad request");
+            else
+                System.out.println("Error: " + errorCode);
+        }
+
+        void login (String input){
             System.out.println("Logging in");
 
             //cut up input
@@ -66,78 +124,66 @@ public class ClientMenu {
             } else {
                 System.out.println("Invalid Input: Login <USERNAME> <PASSWORD>");
             }
-        } else if (input.contains("Register") || input.contains("register")) {
-            System.out.println("Registering User");
-            //cut up input
-            String[] inputData = input.split(" ");
+        }
 
-            if (Arrays.stream(inputData).count() == 4) {
-                try {
-                    authData = serverFacade.makeRequest("POST", "/user", new UserData(inputData[1], inputData[2], inputData[3]), AuthData.class);
-                    System.out.println("Logged in as " + inputData[1]);
 
-                    userStatus = "LOGGED_IN";
-                    identity = "Postlogin UI";
-                } catch (Exception exeption) {
-                    returnErrorMessage(exeption.getMessage());;
-                }
-            } else {
-                System.out.println("Invalid Input: Register <USERNAME> <PASSWORD> <EMAIL>");
+
+    void register(String input){
+        System.out.println("Registering User");
+        //cut up input
+        String[] inputData = input.split(" ");
+
+        if (Arrays.stream(inputData).count() == 4) {
+            try {
+                authData = serverFacade.makeRequest("POST", "/user", new UserData(inputData[1], inputData[2], inputData[3]), AuthData.class);
+                System.out.println("Logged in as " + inputData[1]);
+
+                userStatus = "LOGGED_IN";
+                identity = "Postlogin UI";
+            } catch (Exception exeption) {
+                returnErrorMessage(exeption.getMessage());;
             }
         } else {
-            System.out.println("Invalid Input - Type \"Help\" for a list of commands or \"Quit\" to exit the program.");
+            System.out.println("Invalid Input: Register <USERNAME> <PASSWORD> <EMAIL>");
         }
     }
 
 
-    //Help, Create Game, List Games, Join Game, Join Observer
-    public void postLoginClient() {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
 
-        if (input.contains("Help") || input.contains("help")) {
-            System.out.println("Create <GAMENAME> - Creates a new Game");
-            System.out.println("list - Lists Every Game");
-            System.out.println("Join <ID> [WHITE|BLACK|<empty>] - Joins a game");
-            System.out.println("Observe <ID> - Joins game as a spectator");
-            System.out.println("Logout - Log out of your account");
-            System.out.println("Quit - closes the program");
-            System.out.println("Help - list possible commands");
-        } else if (input.contains("Logout") || input.contains("logout")) {
-            System.out.println("Logging out");
-            userStatus = "LOGGED_OUT";
-            identity = "Prelogin UI";
-        } else if (input.contains("Create") || input.contains("create")) {
-            System.out.println("creating game");
-        } else if (input.contains("List") || input.contains("list")) {
-            System.out.println("listing games");
-        } else if (input.contains("Join") || input.contains("join")) {
-            System.out.println("joining game");
-        } else if (input.contains("Observe") || input.contains("observe")) {
-            System.out.println("observing");
-        } else if (input.contains("Quit") || input.contains("quit")) {
-            //logout
-            identity = "Exit";
-        } else {
-            System.out.println("Invalid Input - Type \"Help\" for a list of commands or \"Quit\" to exit the program.");
-        }
+
+    void logout(){
+        System.out.println("Logging out");
+
+
+        userStatus = "LOGGED_OUT";
+        identity = "Prelogin UI";
     }
 
-    //
-    public void gamePlayClient() {
 
+
+    void create(String input){
+        System.out.println("creating game");
     }
 
-    private void returnErrorMessage(String errorCode) {
-        if (errorCode.contains("400"))
-            System.out.println("Error: bad request");
-        else if (errorCode.contains("401"))
-            System.out.println("Error: unauthorized");
-        else if (errorCode.contains("403"))
-            System.out.println("Error: already taken");
-        else if (errorCode.contains("500"))
-            System.out.println("Error: bad request");
-        else
-            System.out.println("Error: " + errorCode);
+
+
+
+    void list(){
+        System.out.println("listing games");
+    }
+
+
+
+
+    void join(String input){
+        System.out.println("joining game");
+    }
+
+
+
+
+
+    void observe(String input){
+        System.out.println("observing");
     }
 }
